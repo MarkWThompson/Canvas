c7dc6f571bcebc89d4ecb6c33b5de302403d9593
Elliot Morris <elliotcmorris@gmail.com>
2012-05-21 22:45:23 +0100

    added experimental yellow colour

--- Sourcecontroltester/Sourcecontroltester/Sourcecontroltester/Game1.cs
+++ Sourcecontroltester/Sourcecontroltester/Sourcecontroltester/Game1.cs
@@ -81,7 +81,7 @@ namespace Sourcecontroltester
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(Color.Yellow);
 
             // TODO: Add your drawing code here
 
