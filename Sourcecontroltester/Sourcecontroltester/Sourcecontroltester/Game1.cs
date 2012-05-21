c393a563ebaf7c07b3e12da56019aa242377b469
Elliot Morris <elliotcmorris@gmail.com>
2012-05-21 23:00:00 +0100

    brown

--- Sourcecontroltester/Sourcecontroltester/Sourcecontroltester/Game1.cs
+++ Sourcecontroltester/Sourcecontroltester/Sourcecontroltester/Game1.cs
@@ -81,7 +81,7 @@ namespace Sourcecontroltester
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Aquamarine);
+            GraphicsDevice.Clear(Color.Brown);
 
             // TODO: Add your drawing code here
 
