94d435e67eb59b7a9a092249cbe2cd4f711bb310
Mark Thompson <mark.wt93@gmail.com>
2012-05-21 22:20:00 +0100

    Changed BG to black

--- Sourcecontroltester/Sourcecontroltester/Sourcecontroltester/Game1.cs
+++ Sourcecontroltester/Sourcecontroltester/Sourcecontroltester/Game1.cs
@@ -81,7 +81,7 @@ namespace Sourcecontroltester
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Green);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
